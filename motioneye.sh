15e98ad615edd36a71d04862ce4fa362d2355c1e
glows <lawsunlie@gmail.com>
2020-05-28 17:56:11 +0800

    update motion eye

---
+++ motioneye.sh
@@ -0,0 +1,10 @@
+docker run --name="motioneye" \
+    -p 8765:8765 \
+    --hostname="motioneye" \
+    -v /etc/localtime:/etc/localtime:ro \
+    -v /etc/motioneye:/etc/motioneye \
+    -v /var/lib/motioneye:/var/lib/motioneye \
+    --device=/dev/video0
+    --restart="always" \
+    --detach=true \
+    ccrisan/motioneye:master-amd64
